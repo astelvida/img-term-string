b410bca8953dd734209f46a0003b4ad25daf65b4
astelvida <sevda.m.anefi@gmail.com>
2018-03-03 06:10:54 +0000

    update example.js

--- example.js
+++ example.js
@@ -1,17 +1,19 @@
-const imgToTerm = require('./index.js')
+const imgToTerm = require('./index.js');
 
-imgToTerm({ src: 'https://media.giphy.com/media/l0MYKZKh3R6LdBvEc/giphy.gif' }, (body) => {
-    console.log('auto\n', body)
+imgToTerm({ src: 'https://media.giphy.com/media/l0MYKZKh3R6LdBvEc/giphy.gif' }, body => {
+        console.log(body);
 });
 
-imgToTerm({ src: 'https://media.giphy.com/media/gx54W1mSpeYMg/giphy.gif'}).then((body) => {
-    console.log('Promise me auto\n', body)
-});
+imgToTerm({ src: 'https://media.giphy.com/media/gx54W1mSpeYMg/giphy.gif' })
+    .then(body => {
+        console.log(body);
+    });
 
-imgToTerm({ src: 'ohno.jpg', width: '100px' }).then(body => {
-    console.log('Promiseeeee \n', body)
-});
+imgToTerm({ src: 'ohno.jpg', width: '100px' })
+    .then(body => {
+        console.log(body);
+    });
 
-imgToTerm({ src: 'oopsie.gif', height: '200px', width: 'auto' }, (body) => {
-    console.log('oopsie\n', body)
+imgToTerm({ src: 'oopsie.gif', height: '200px', width: 'auto' }, body => {
+    console.log(body);
 });
